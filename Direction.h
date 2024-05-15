42ade817ea636685a601de87475d3b31752ed76a
ZYF <1599838323@qq.com>
2024-05-15 18:12:37 +0800

    Update Direction.h

--- Direction.h
+++ Direction.h
@@ -1,1 +1,12 @@
+#ifndef DIRECTIONS_H
+#define DIRECTIONS_H
+//因为mainwindow与snak类中都要用到Direction类变量，单开一类存放
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+    None
+};
+#endif // DIRECTIONS_H
 
